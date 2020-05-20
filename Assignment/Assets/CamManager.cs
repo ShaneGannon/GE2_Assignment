c94f81c34d49f701e77d6bbfc35994b7eb6c526e
Shane Gannon <shane.gannon.halo@gmail.com>
2020-05-20 20:07:51 +0100

    have most of the story implemented, some timing tweaks and code cleanup required

---
+++ Assignment/Assets/CamManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamManager : MonoBehaviour
+{
+    public Camera introcam;
+    public Camera HQCamera;
+
+    // Start is called before the first frame update
+
+
+    IEnumerator Switch()
+    { 
+            yield return new WaitForSeconds(25);
+            introcam.enabled = false;
+            HQCamera.enabled = true;       
+
+    }
+
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        introcam.enabled = true;
+        HQCamera.enabled = false;
+        StartCoroutine(Switch());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assignment/Assets/Scenemanag.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenemanag : MonoBehaviour
+{
+    bool change;
+    public int SceneTime;
+    public string NextScene;
+    IEnumerator Swap()
+    {
+        yield return new WaitForSeconds(SceneTime);
+        //SceneManager.LoadScene("ReactionScene");
+        change = true;
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        change = false;
+        StartCoroutine(Swap());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(change == true)
+        {
+            SceneManager.LoadScene(NextScene, LoadSceneMode.Single);
+            //SceneManager.SetActiveScene(ReactionScene);
+        }
+    }
+}
